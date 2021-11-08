5671b865b50377fafe9d799fb29ec04c9a4c2be7
Abhey <abhey@Abheys-MacBook-Air.local>
2021-11-08 13:58:36 +0530

    updates

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -62,7 +62,7 @@ export default function Login() {
         <ContentStyle>
           <Stack sx={{ mb: 5 }}>
             <Typography variant="h4" gutterBottom>
-              Sign in to IBM Admin Panel
+              Sign in to IBS Admin Panel
             </Typography>
             <Typography sx={{ color: 'text.secondary' }}>Enter your details below.</Typography>
           </Stack>
